2c391afb1e95cca73ebd2a50ecb1828c4159619c
Bluskyfishing <blusky445@gmail.com>
2024-09-15 01:35:54 +0200

    added data for db to test.

---
+++ BookStoreAPI/SetupDB/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO Book (Title, Author, PublicationYear, ISBN, InStock)
+VALUES
+('To Kill a Mockingbird', 'Harper Lee', 1960, '9780061120084', 5),
+('1984', 'George Orwell', 1949, '9780451524935', 7),
+('Pride and Prejudice', 'Jane Austen', 1813, '9781503290563', 3),
+('The Great Gatsby', 'F. Scott Fitzgerald', 1925, '9780743273565', 6),
+('Moby-Dick', 'Herman Melville', 1851, '9781503280786', 2),
+('War and Peace', 'Leo Tolstoy', 1869, '9780199232765', 4),
+('The Catcher in the Rye', 'J.D. Salinger', 1951, '9780316769488', 8),
+('The Hobbit', 'J.R.R. Tolkien', 1937, '9780547928227', 10),
+('Ulysses', 'James Joyce', 1922, '9780199535675', 1),
+('The Odyssey', 'Homer', -800, '9780140268867', 9);
